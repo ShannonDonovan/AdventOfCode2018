f9bab819221d3e4f93fe9637b6f4f7b5126cf7de
Shannon <shannon.donovan1995@gmail.com>
2019-11-05 17:41:40 -0800

    day 1 part 2

---
+++ Day1Part2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace advent1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var flag = false;
+            var count = 0;
+            var input = File.ReadAllLines("txt.txt").Select(Int32.Parse);
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            while (flag == false)
+            {
+                foreach (var line in input)
+                {
+                    count += line;
+                    if (!dict.ContainsKey(count))
+                    {
+                        dict.Add(count, count);
+                    }
+                    else
+                    {
+                        flag = true;
+                        Console.WriteLine("got here");
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
