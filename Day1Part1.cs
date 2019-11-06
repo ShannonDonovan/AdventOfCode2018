2f2552a2ffbd80c8232c224adb5836e00381ce3f
Shannon <shannon.donovan1995@gmail.com>
2019-11-05 17:18:58 -0800

    Advent day one part one

---
+++ Day1Part1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace advent1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines("txt.txt").Select(Int32.Parse).Sum();
+            Console.WriteLine(input);
+        }
+    }
+}
